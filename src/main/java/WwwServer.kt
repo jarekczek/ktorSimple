d9b1f9b4e0283b5231a9b0e657ff989a42360d20
jarekczek <jarekczek@poczta.onet.pl>
2018-04-17 22:00:07 +0200

    ssl connector

--- src/main/java/WwwServer.kt
+++ src/main/java/WwwServer.kt
@@ -1,25 +1,57 @@
-import java.util.concurrent.TimeUnit
-import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.applicationEngineEnvironment
+import io.ktor.server.engine.connector
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.engine.sslConnector
+import io.ktor.server.netty.Netty
+import java.security.KeyStore
 
 fun main(args: Array<String>) {
   println("hello")
-  val server = embeddedServer(Netty, 8080) {
+
+  class DummyClass {}
+
+  val module: Application.() -> Unit = {
     routing {
       get("/") {
-        call.respondText("Hello, world 8080!", ContentType.Text.Html)
+        call.respondText("Hello, module world 8081!", ContentType.Text.Html)
       }
     }
   }
+
+  val keyStore = KeyStore.getInstance("JKS")
+  keyStore.load(DummyClass::class.java.getResourceAsStream("WwwServerKt.jks"), "changeit".toCharArray())
+  println("keystore: " + keyStore)
+  println("keystore aliases: " + keyStore.aliases().toList().joinToString(","))
+
+  val env = applicationEngineEnvironment {
+    connector {
+      port = 8080
+    }
+    sslConnector(
+      keyStore,
+      "keyali",
+      { "changeit".toCharArray() },
+      { "changeit".toCharArray() },
+      {
+        port = 8081
+        keyAlias = "server"
+      }
+    )
+    modules.add(module)
+  }
+
+  val server = embeddedServer(Netty, env)
   println("starting server")
   server.start(wait = false)
   println("server started")
   Thread.sleep(10*1000)
-  println("stoping server")
-  server.stop(1, 1, TimeUnit.SECONDS)
-  println("server stoped")
+  //println("stoping server")
+  //server.stop(1, 1, TimeUnit.SECONDS)
+  //println("server stoped")
 }
